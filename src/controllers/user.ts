7505c7e5da205be26ef5bd4e2b38ae4319d04dd4
Zaman Ehsani <zamanehsani@gmail.com>
2025-01-01 17:09:52 +0400

    user creation, admin user creation, validation and login is done

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -54,26 +54,58 @@ export const login = async (req: Request, res: Response) => {
 };
 
 export const addUser = async (req: Request, res: Response) => {
-  // validate the request
-  const auth = req.headers.authorization;
-  if (!auth) {
-    res.status(401).json({ error: "Not authorized" });
-    return;
-  }
-  // validate the token
-  const token = auth.split(" ")[1];
-  if (!token) {
-    res.status(401).json({ error: "unauthorized, no token provided." });
-  }
-
-  //   validate the premissions
-  const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-  if (!decoded) {
-    res.status(403).json({ error: "forbidden" });
-  }
-
   try {
-    const user = 0;
+    // validate the request
+    const auth = req.headers.authorization;
+    if (!auth) {
+      res.status(401).json({ error: "Not authorized" });
+      return;
+    }
+    // validate the token
+    const token = auth.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ error: "unauthorized, no token provided." });
+    }
+
+    //   validate the premissions
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    const decodedToken = decoded as jwt.JwtPayload & { user: any };
+
+    if (!decoded || !decodedToken.user) {
+      res.status(403).json({ error: "forbidden" });
+      return;
+    }
+
+    // if inside decoded. user.roles includes "admin" then create the user
+
+    if (!decodedToken.user?.roles.includes("admin")) {
+      res.status(403).json({ error: "forbidden" });
+      return;
+    }
+    console.log("creating user");
+
+    const { name, email, password, phone, roles } = req.body;
+    if (!name || !email || !password || !phone || !roles) {
+      res.status(400).json({ error: "Please provide all the required fields" });
+      return;
+    }
+
+    // check if the email is not taken
+    const userExists = await prisma.users.findUnique({
+      where: { email },
+    });
+
+    if (userExists) {
+      res.status(400).json({ error: "Email already taken" });
+      return;
+    }
+    const passwordHashed = await bcrypt.hash(password, 10);
+
+    const user = await prisma.users.create({
+      data: { name, email, password: passwordHashed, phone, roles },
+    });
+
+    // const user = 0;
     res.status(201).json(user);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
