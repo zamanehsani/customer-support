88bc0e9408c10cd52d7a54eb1e876daea7fb02b9
Zaman Ehsani <zamanehsani@gmail.com>
2025-01-01 16:30:44 +0400

    added uniques contrains on email at Users and Clients model. Also created the user login controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,6 +4,32 @@ import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
+export const login = async (req: Request, res: Response) => {
+  try {
+    // first check if reqeust body is not empty
+    if (!req.body || Object.keys(req.body).length === 0) {
+      throw new Error("Please provide the user data");
+    }
+    // get the requst name, email, and password
+    const { email, password } = req.body;
+
+    console.log("req body is was not empty. continue...", email, password);
+    // check if the email and password are provided
+    if (!email || !password) {
+      throw new Error("Please provide email and password");
+    }
+    // check if the email is valid
+    if (!email.includes("@")) {
+      throw new Error("Please provide a valid email");
+    }
+
+    console.log("email and passwoue...");
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+};
+
 export const addUser = async (req: Request, res: Response) => {
   // validate the request
   const auth = req.headers.authorization;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { addUser } from "../controllers/user";
+import { addUser, login } from "../controllers/user";
 
 export const user_routes = express.Router();
 user_routes.post("/", addUser);
+user_routes.post("/login", login);
